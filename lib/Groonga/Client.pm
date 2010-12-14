86a89b46779271e9054f04ac7715c22ea961f06a
okamuuu <okamuuuuu@gmail.com>
2010-12-14 23:54:38 +0900

    initial commit

---
+++ lib/Groonga/Client.pm
@@ -0,0 +1,32 @@
+package Groonga::Client;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Groonga::Client -
+
+=head1 SYNOPSIS
+
+  use Groonga::Client;
+
+=head1 DESCRIPTION
+
+Groonga::Client is
+
+=head1 AUTHOR
+
+okamuuu E<lt>okamuuu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
